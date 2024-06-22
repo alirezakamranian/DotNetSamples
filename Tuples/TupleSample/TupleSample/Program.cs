a8d648586062bab1c0d5f2f9d35663d0d7af5891
AlirezaKamranian <123934502+alirezakamranian@users.noreply.github.com>
2024-06-22 05:08:18 -0700

    Feat:Add tupleSample

---
+++ Tuples/TupleSample/TupleSample/Program.cs
@@ -0,0 +1,21 @@
+﻿//Define
+
+(string, int, string) person = ("sina", 19, "mahdi");
+
+var person2 = new Tuple<string, int, string>("ali",13,"mehrdad");
+
+var person3 = Tuple.Create("bahraam","ISTJ",23);
+
+
+// very simple sample
+
+    (int,string) GetChangedUserDetails(string name , int age)
+    {
+        return (age*10,"hello "+name);
+    }
+
+
+var res = GetChangedUserDetails("ali", 15);
+
+Console.WriteLine(res.Item1.ToString()+" " +res.Item2);
+
